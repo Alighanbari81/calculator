e60c6219611944147bcc5087867af664f744a567
Ali Ghanabri <alighanbari381@gmail.com>
2023-06-25 11:19:48 +0330

    add new python file and write simple code calculator

---
+++ calculator.py
@@ -0,0 +1,34 @@
+
+# Define function for addition
+def add(x, y):
+    return x + y
+ # Define function for subtraction
+def subtract(x, y):
+    return x - y
+ # Define function for multiplication
+def multiply(x, y):
+    return x * y
+ # Define function for division
+def divide(x, y):
+    return x / y
+ # Take user input for operation choice
+print("Select operation.")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+choice = input("Enter operation choice (1/2/3/4): ")
+ # Take user input for numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+ # Perform operation based on user choice
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
+else:
+    print("Invalid input")
